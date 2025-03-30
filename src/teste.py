df168efc98d66b5cdda61ac5bf495085eba8fa1e
sxtroq <vieiralucca20191@gmail.com>
2025-03-30 10:19:33 -0300

    testando

---
+++ src/teste.py
@@ -0,0 +1,12 @@
+
+def minha_funçao(valor1,valor2):
+    return valor1 + valor2
+while True:
+    
+    valor1 = int(input("valor1"))
+
+    valor2 = int(input("valor2"))
+
+resposta = minha_funçao(10,10)
+
+print(valor1, "+" , valor2, "=" ,resposta)
