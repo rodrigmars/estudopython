e5bab93ad9f2bade049582e192c2b6e1fab5dfd0
sxtroq <vieiralucca20191@gmail.com>
2025-03-30 00:11:15 -0300

    teste

--- src/cabritogames.py
+++ src/cabritogames.py
@@ -1,1 +1,29 @@
-print("testeandoepeidando")
+def somar(a, b):
+
+    return a + b
+
+
+def subtrair(a, b):
+
+    return a - b
+
+
+def multiplicar(a, b):
+
+    return a * b
+
+
+def dividir(a, b):
+
+    return a / b
+
+
+resp_somar = somar(210, 89)
+resp_sub = subtrair(10, 32)
+resp_mult = multiplicar(2896, 87849)
+resp_divisao = dividir(2, 7)
+
+print(f"somar: {resp_somar}\n")
+print(f"subtrair: {resp_sub}\n")
+print(f"multiplicar: {resp_mult}\n")
+print(f"dividir: {resp_divisao}\n")
